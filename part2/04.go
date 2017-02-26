012c08b4f0ae975c97614eacddc75f02788868c3
John Crisostomo <johncrisostomo@protonmail.ch>
2017-02-26 16:58:15 +0800

    finishes 04.go

---
+++ part2/04.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  /*
+    What is the value of this Boolean expression (true && false) || (false && true) || !(false && false) ?
+  */
+  fmt.Println("true")
+}
