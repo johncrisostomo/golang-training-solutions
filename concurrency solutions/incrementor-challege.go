6ee285db0c2e74414ceecf8b08de61ba994898db
John Crisostomo <johncrisostomo@protonmail.ch>
2017-03-06 22:58:13 +0800

    finishes incrementor challenge

--- concurrency solutions/incrementor-challege.go
+++ concurrency solutions/incrementor-challege.go
@@ -2,25 +2,40 @@ package main
 
 import (
   "fmt"
-  "sync"
-  "sync/atomic"
 )
 
 var count int64
-var wg sync.WaitGroup
 
 func main() {
-  wg.Add(2)
-  go incrementor("1")
-  go incrementor("2")
-  wg.Wait()
+  c := incrementor(2)
+  var count int
+
+  for s := range c {
+    count++  
+    fmt.Println(s)
+  }
   fmt.Println("Final Counter:", count)
 }
 
-func incrementor(s string) {
-  for i := 0; i < 20; i++ {
-    atomic.AddInt64(&count, 1)
-    fmt.Println("Process: " +s+ " printing", i)
+func incrementor(n int) chan string {
+  c := make(chan string)
+  done := make(chan bool)
+
+  for i := 1; i <= n; i++ {
+    go func(j int) {
+      for k := 0; k < 20; k++ {
+        c <-fmt.Sprintf("Process: %d printing: %d", j, k)    
+      }
+      done <-true
+    }(i)
   }
-  wg.Done()
+
+  go func() {
+    for i := 0; i < n; i++ {
+      <-done
+    }
+    close(c)
+  }()
+
+  return c
 }
